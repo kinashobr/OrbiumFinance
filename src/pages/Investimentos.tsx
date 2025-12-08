e317323b0c947b020000f0cc37b2041c38b9b743
Gabriel <gabrielschimtez@gmail.com>
2025-12-07 23:56:55 -0300

    [dyad] Fixing Renda Fixa movimentações functionality - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Investimentos.tsx
+++ src/pages/Investimentos.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useMemo, useCallback } from "react";
 import { MainLayout } from "@/components/layout/MainLayout";
 import { Button } from "@/components/ui/button";
